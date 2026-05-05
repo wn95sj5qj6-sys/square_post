3f38416b8166c55bdbc0561697d6328b3eac0d18
wn95sj5qj6-sys <wn95sj5qj6@privaterelay.appleid.com>
2026-05-05 11:50:37 +0800

    Update post_main.py

--- post_main.py
+++ post_main.py
@@ -2,16 +2,16 @@ import requests
 
 def post_content(content, api_key):
     try:
-        url = "https://www.binance.com/bapi/composite/v1/public/pgc/openApi/content/add"
         headers = {
             "X-Square-OpenAPI-Key": api_key.strip(),
             "Content-Type": "application/json"
         }
         data = {"bodyTextOnly": content}
-        r = requests.post(url, headers=headers, json=data, timeout=15)
+        r = requests.post("https://www.binance.com/bapi/composite/v1/public/pgc/openApi/content/add",
+                          headers=headers, json=data, timeout=15)
         j = r.json()
         if j.get("success"):
-            return True, "文章ID："+str(j.get("data")), j.get("data")
+            return True, "成功", j.get("data", "")
         return False, str(j), ""
     except Exception as e:
         return False, str(e), ""
